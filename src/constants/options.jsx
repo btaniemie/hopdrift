5a90197b7a7c65ccdd5b6400ffe052fd08b07cde
eltammie <lennminh@gmail.com>
2024-07-18 12:06:42 +0700

    added basic trip info form

---
+++ src/constants/options.jsx
@@ -0,0 +1,53 @@
+export const selectTravellerList= [
+    {
+        id: 1,
+        title: 'Just Me',
+        desc: 'Perfect for solo adventurers looking for a personalized getaway.',
+        icon: '🧳',
+        people: '1'
+    },
+    {
+        id: 2,
+        title: 'Lover',
+        desc: 'Ideal for romantic escapes and special moments together.',
+        icon: '👩🏻‍❤️‍👨🏻',
+        people: '2 people'
+    },
+    {
+        id: 3,
+        title: 'Family',
+        desc: 'Tailored for family fun, with activities for all ages.',
+        icon: '👨‍👩‍👧‍👦',
+        people: '3-5 people'
+    },
+    {
+        id: 4,
+        title: 'Friends',
+        desc: 'Great for group adventures and making unforgettable memories with friends.',
+        icon: '🧿',
+        people: '5-10 people'
+    },
+]
+
+export const selectBudgetList = [
+    {
+        id: 1,
+        title: 'Affordable',
+        desc: 'Enjoy an amazing trip without breaking the bank.',
+        icon: '💰'
+    },
+    {
+        id: 2,
+        title: 'Mid-range',
+        desc: 'Balance comfort and cost for a well-rounded experience.',
+        icon: '💵'
+    },
+    {
+        id: 3,
+        title: 'Luxury',
+        desc: 'Indulge in premium experiences and top-notch amenities.',
+        icon: '💸'
+    },
+]
+
+export const AI_PROMPT = 'Generate travel plans for location: {location}'
